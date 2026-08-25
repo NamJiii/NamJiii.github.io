b9648c1baa0d5a7456d1910d5d90a2c09b3dc945
NamJiii <NamJiii@users.noreply.github.com>
2026-08-25 13:35:15 +0000

    deploy: NamJiii/github.io@d829a1e02603870efe63c776c4e3ec75bbd7185b

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "[DWR]260825",
-        "excerpt":"Summary Done: 20T Weekly: 40 / 100T Focus: 0h 00m Work: 9h 11m Done [4T] Daily Routine — triage, planning, updates &amp; admin [1T] SPL 실험 진행상황 체크 이슈 발생시 대응 [5T] Github 블로그 제작 및 초기 세팅 [2T] Daily Scrum 규칙 수립 [8T] 자기개발 (CKH1) Next [4T] Daily Routine...","categories": ["Dailyscrum"],
+        "excerpt":"Summary Done: 25T Weekly: 45 / 100T Focus: -h –m Work: 11h 41m Done [4T] Daily Routine — triage, planning, updates &amp; admin [1T] SPL 실험 진행상황 체크 이슈 발생시 대응 [5T] Github 블로그 제작 및 초기 세팅 [2T] Daily Scrum 규칙 수립 [13T] 자기개발 (CKH1) Next [4T] Daily Routine...","categories": ["Dailyscrum"],
         "tags": [],
         "url": "/dailyscrum/260825/",
         "teaser": null
