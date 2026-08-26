0650b7ecc8d012a0fecfd68336be7a88989454ef
NamJiii <NamJiii@users.noreply.github.com>
2026-08-26 09:41:38 +0000

    deploy: NamJiii/github.io@e1232c84329a6e773cf4d9a065a8484e36b55132

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "[DWR]260825",
-        "excerpt":"Summary Done: 25T Weekly: 45 / 100T Focus: -h –m Work: 11h 41m Done [4T] Daily Routine — triage, planning, updates &amp; admin [1T] SPL 실험 진행상황 체크 이슈 발생시 대응 [5T] Github 블로그 제작 및 초기 세팅 [2T] Daily Scrum 규칙 수립 [13T] 자기개발 (CKH1) Next [4T] Daily Routine...","categories": ["Dailyscrum"],
+        "excerpt":"Summary Done: 25T Weekly: 41 / 80T Focus: -h –m Work: 11h 41m Done [4T] Daily Routine — triage, planning, updates &amp; admin [1T] SPL 실험 진행상황 체크 이슈 발생시 대응 [5T] Github 블로그 제작 및 초기 세팅 [2T] Daily Scrum 규칙 수립 [13T] 자기개발 (CKH1) Next [4T] Daily Routine...","categories": ["Dailyscrum"],
         "tags": [],
         "url": "/dailyscrum/260825/",
+        "teaser": null
+      },{
+        "title": "[DWR]260826",
+        "excerpt":"Summary Done: 18T Weekly: 59 / 80T Focus: -h –m Work: -h -m Done [4T] Daily Routine — triage, planning, updates &amp; admin [2T] SPL 실험 진행상황 체크 이슈 발생시 대응 [6T] 자기개발 (CHM0) [6T] 업무처리 (KJKY) Next [4T] Daily Routine — triage, planning, updates &amp; admin [8T] 재개발 스터디...","categories": ["Dailyscrum"],
+        "tags": [],
+        "url": "/dailyscrum/260826/",
         "teaser": null
       }]
