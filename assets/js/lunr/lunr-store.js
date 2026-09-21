ce463f80fce31437556562f08ad0ad1cf8fbf632
NamJiii <NamJiii@users.noreply.github.com>
2026-09-21 10:07:11 +0000

    deploy: NamJiii/github.io@7de4bfe9c31bb134c341aff8ff0e74491b08c508

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -136,4 +136,10 @@ var store = [{
         "tags": [],
         "url": "/dailyscrum/260920/",
         "teaser": null
+      },{
+        "title": "[DWR]260921",
+        "excerpt":"Summary Done: 18T Weekly: 18 / 80T Focus: -h –m Work: -h -m Done [4T] Daily Routine — triage, planning, updates &amp; admin [4T] 자기개발(CNB2) [10T] 자기개발(CHK1) Next [4T] Daily Routine — triage, planning, updates &amp; admin [2T] 재개발 스터디 - 주요개념 정리 [6T] 재개발 스터디 - 가치산정 방식 명확화...","categories": ["Dailyscrum"],
+        "tags": [],
+        "url": "/dailyscrum/260921/",
+        "teaser": null
       }]
